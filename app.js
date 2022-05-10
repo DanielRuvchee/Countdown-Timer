ec5348b65d17554d8b280a8f1c7ff1d209e4ae8c
Daniel Ruvcheski <famruvcheski@gmail.com>
2022-05-10 21:35:27 +0200

    Finish Demo

---
+++ app.js
@@ -0,0 +1,31 @@
+const daysElements = document.getElementById('days');
+const hoursElements = document.getElementById('hours');
+const minsElements = document.getElementById('minutes');
+const secondsElements = document.getElementById('seconds');
+
+const newYears = '1 January 2023';
+
+function countDown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds =(newYearsDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysElements.innerHTML = days;
+    hoursElements.innerHTML = formatTime(hours);
+    minsElements.innerHTML = formatTime(minutes);
+    secondsElements.innerHTML = formatTime(seconds);
+}
+
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+//Initial Count
+countDown();
+setInterval(countDown, 1000);
